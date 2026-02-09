6ca69a09d2a8c0206cec2f6e1cac1814596e6daf
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2026-02-09 21:57:49 +0600

    📕🛠️added

---
+++ src/constants/config.ts
@@ -0,0 +1,13 @@
+
+export const CONFIG = {
+  // Mobile App Availability
+  IS_ANDROID_APP_PUBLIC: false,
+  IS_IOS_APP_PUBLIC: false,
+
+  // App URLs
+  ANDROID_APP_URL: 'https://play.google.com/store/apps/details?id=dev.akhlak.app.pulsethread',
+  ANDROID_EARLY_ACCESS_URL: 'https://forms.gle/ortGgg55PFbcuk5V8',
+  
+  IOS_APP_URL: 'https://apps.apple.com/us/app/pulse-thread/...',
+  IOS_WISHLIST_URL: 'https://forms.gle/AF46xcRqEB2AAtwg9',
+};
